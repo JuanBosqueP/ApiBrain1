eb1747740d7f14d19981b1130b00857b1fe301f6
DanielG <danielfelipegonza123@gmail.com>
2024-11-24 16:21:10 -0500

    Commit 13
    Pagina de estudiante

---
+++ pagina web/js/estudiante.js
@@ -0,0 +1,12 @@
+function showSection(sectionId) {
+    // Ocultar todas las secciones
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => section.classList.remove('active'));
+  
+    // Mostrar la sección seleccionada
+    const sectionToShow = document.getElementById(sectionId);
+    if (sectionToShow) {
+      sectionToShow.classList.add('active');
+    }
+  }
+  
